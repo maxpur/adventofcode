1c6b46ef0103350ba930ee743b8a6827098b42ce
Maximilian Purrucker <m.purrucker@tu-braunschweig.de>
2023-01-16 15:20:53 +0100

    completed task 8

--- task8/task8.py
+++ task8/task8.py
@@ -1,3 +1,4 @@
+# Parse file into multi-dimensional list
 def pre_process_file(file):
     data = list()
     for line in file:
@@ -6,15 +7,18 @@ def pre_process_file(file):
     return data
 
 
+# Calculate all visible trees as required in task
 def count_visible_trees(file):
     grid = pre_process_file(file)
     return count_edge_trees(grid) + count_interior_trees(grid)
 
 
+# Count trees at the edge of the grid
 def count_edge_trees(data):
     return 2 * len(data) + len(data[0]) + len(data[len(data) - 1]) - 4
 
 
+# Count trees in between the the edges of the grid
 def count_interior_trees(data):
     counter = 0
     for i in range(1, len(data) - 1):
@@ -24,6 +28,7 @@ def count_interior_trees(data):
     return counter
 
 
+# Check if tree is visible in horizontal orientation
 def is_horizontal_visible(data, row, column):
     height = data[row][column]
     visible_left = True
@@ -35,6 +40,7 @@ def is_horizontal_visible(data, row, column):
     return visible_left or visible_right
 
 
+# Check if tree is visible in vertical orientation
 def is_vertical_visible(data, row, column):
     height = data[row][column]
     visible_up = True
@@ -46,6 +52,7 @@ def is_vertical_visible(data, row, column):
     return visible_up or visible_down
 
 
+# Calculate max scenic score as defined in task
 def compute_scenic_score(file):
     data = pre_process_file(file)
     maximum = 0
@@ -56,12 +63,13 @@ def compute_scenic_score(file):
     return maximum
 
 
+# Calculate vertical scenic of grid
 def compute_vertical_distance(data, row, column):
     distance_up = 0
     distance_down = 0
     height = data[row][column]
 
-    for i in range(row - 1, 0, -1):
+    for i in range(row - 1, -1, -1):
         if data[i][column] < height:
             distance_up += 1
         elif data[i][column] >= height:
@@ -76,12 +84,12 @@ def compute_vertical_distance(data, row, column):
     return distance_up * distance_down
 
 
+# Calculate horizontal scenic of grid
 def compute_horizontal_distance(data, row, column):
     distance_left = 0
     distance_right = 0
     height = data[row][column]
-
-    for i in range(column - 1, 0, -1):
+    for i in range(column - 1, -1, -1):
         if data[row][i] < height:
             distance_left += 1
         elif data[row][i] >= height:
@@ -90,7 +98,7 @@ def compute_horizontal_distance(data, row, column):
     for i in range(column + 1, len(data[row])):
         if data[row][i] < height:
             distance_right += 1
-        elif data[i][column] >= height:
+        elif data[row][i] >= height:
             distance_right += 1
             break 
     return distance_left * distance_right
@@ -99,8 +107,8 @@ def compute_horizontal_distance(data, row, column):
 def main():
     file = open("task8.txt", "r")
     print(f'Solution star one: {count_visible_trees(file)}')
-    # file = open("test.txt", "r")
-    # print(f'Solution star two: {compute_scenic_score(file)}')
+    file = open("task8.txt", "r")
+    print(f'Solution star two: {compute_scenic_score(file)}')
 
 
 if __name__ == "__main__":
